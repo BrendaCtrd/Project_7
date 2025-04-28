d06bcfa364b1c49e2bde0a694704f90e0342a985
BrendaCtrd <brendactrdz@hotmail.com>
2025-04-28 17:53:16 -0600

    Envío final de archivos

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Project_7
 Project 7. Software development tools
+
+En este proyecto, se puede visualizar una gráfica de dispersión entre la condición y el precio de los carros disponibles mediante un botón interactivo.
+También se incluye una opción, mediante una casilla de verificación (checkbox), para generar un histograma que muestra la distribución de los tipos de carro.
+Finalmente, se incorpora un slider de doble extremo que actúa como filtro, permitiendo visualizar los modelos de carros disponibles dentro de un rango de precios determinado.
+En conjunto, estas herramientas permiten al usuario analizar la distribución y características de los vehículos disponibles de forma visual y dinámica.
--- app.py
+++ app.py
@@ -1,19 +1,42 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
+import streamlit as st
 
-st.title("Mi primera app en Render ✨")
-st.write("¡Hola, mundo!")
+st.header('Car Sales', divider=True)
+        
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+      
 
-# Ejemplo de tabla
-data = pd.DataFrame({
-    "Categoría": ["A", "B", "C"],
-    "Valor": [10, 20, 30]
-})
+#Creación de botón para gráfico de dispersión
+scatterpl_button = st.button('Build scatter plot') # crear un botón
 
-st.write("Tabla de ejemplo:")
-st.dataframe(data)
+if scatterpl_button: # al hacer clic en el botón
+            # escribir un mensaje
+    st.write('Scatter plot of condition of cars for sale vs. price')
+    
+    fig2 = px.scatter(car_data, x='condition', y='price', color='condition', labels={'condition': 'Car condition', 'price': 'Price $USD'})
+    st.plotly_chart(fig2, use_container_width=True)
 
-# Ejemplo de gráfico
-fig = px.bar(data, x="Categoría", y="Valor", title="Gráfico de barras")
-st.plotly_chart(fig)
+build_histogram = st.checkbox('Build cars on sale histogram')
+
+if build_histogram:
+    st.write('Type of cars on sale histogram')
+            
+    # crear un histograma
+    fig = px.histogram(car_data, x="type", color="type", labels={'type': 'Car type'})
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+st.subheader("Cars available per price")
+
+# Slider de rango de precios
+slider_range = st.slider("Price filter", 0, 375000, (0, 375000))  
+
+# Filtrar el DataFrame por el rango seleccionado
+filtered_data = car_data[(car_data['price'] >= slider_range[0]) & (car_data['price'] <= slider_range[1])]
+
+# Mostrar los modelos disponibles en ese rango
+st.write("The available cars are:")
+st.write(filtered_data['model'].unique())
+
